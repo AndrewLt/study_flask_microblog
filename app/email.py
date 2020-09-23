f471d082dc4b5a1b4951308324d4cc46fe01855f
AndrewLt <Andrew777ok@gmail.com>
2020-09-23 16:05:38 +0300

    v10

---
+++ app/email.py
@@ -0,0 +1,53 @@
+from threading import Thread
+from flask import render_template
+#from flask_mail import Message
+from app import app
+import requests
+
+"""Test module. The module send jwt token to my Telegram account"""
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        #mail.send(msg)
+        pass
+
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = f'<b>{subject}</b>\nFrom: {sender}\n' \
+          f'To: {recipients}\n\n{text_body}\n{html_body}'
+    token = app.config['TELEGRAM_TOKEN']
+    admin_chat_id = app.config['TELEGRAM_ADMIN_CHAT_ID']
+    requests.get(f'https://api.telegram.org/bot{token}/'
+                 f'sendMessage?chat_id={admin_chat_id}&text={msg}&parse_mode=HTML')
+    #Thread(target=send_async_email, args=(app, msg)).start()
+
+
+def send_password_reset_email(user):
+    token = user.get_reset_password_token()
+    send_email('[Microblog] Reset Your Password',
+               sender=app.config['ADMINS'][0],
+               recipients=[user.email],
+               text_body=render_template('email/reset_password.txt',
+                                         user=user, token=token),
+               html_body=render_template('email/reset_password.html',
+                                         user=user, token=token))
+
+"""
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    mail.send(msg)
+
+
+def send_password_reset_email(user):
+    token = user.get_reset_password_token()
+    send_email('[Microblog] Reset Your Password',
+               sender=app.config['ADMINS'][0],
+               recipients=[user.email],
+               text_body=render_template('email/reset_password.txt',
+                                         user=user, token=token),
+               html_body=render_template('email/reset_password.html',
+                                         user=user, token=token))
+"""
--- config.py
+++ config.py
@@ -7,3 +7,12 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 21
+    TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+    TELEGRAM_ADMIN_CHAT_ID = os.environ.get('TELEGRAM_ADMIN_CHAT_ID')
+    MAIL_SERVER = 'localhost'  # 'smtp.googlemail.com' os.environ.get('MAIL_SERVER')
+    MAIL_PORT = 8025  # int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = 1  # os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = 'andrii'  # os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = '212'  # os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['admin-andrew@example.com']
